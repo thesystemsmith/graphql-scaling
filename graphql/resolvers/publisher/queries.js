55986e78d8675816d5f04b3b17525ce8d54269ed
abdul rasheed <abdul.rasheed@simpleenergy.in>
2025-03-24 16:50:32 +0530

    [feat]: pagination

--- graphql/resolvers/publisher/queries.js
+++ graphql/resolvers/publisher/queries.js
@@ -1,16 +1,39 @@
 import { Publisher } from '../../../db/models';
 
 const publisherQueries = {
-    publishers: async (_, args) => {
-        const publishers = await Publisher.find();
-
-        return publishers;
-    },
-    publisher: async (_, { id }) => {
-        const publisher = await Publisher.findById(id);
-
-        return publisher;
-    },
+    publishers: async (
+        _,
+        { params = { page: 1, pageSize: 20 } },
+        { loaders }
+      ) => {
+        const { pageSize, page } = params;
+    
+        return {
+          results: async () => {
+            const publishers = await Publisher.find()
+              .skip(pageSize * (page - 1))
+              .limit(pageSize);
+    
+            return loaders.publisher.many(publishers.map(({ id }) => id));
+          },
+          info: async () => {
+            const count = await Publisher.countDocuments();
+    
+            const pages = Math.ceil(count / pageSize);
+            const prev = page > 1 ? page - 1 : null;
+            const next = page < pages ? page + 1 : null;
+    
+            return {
+              count,
+              pages,
+              prev,
+              next,
+            };
+          },
+        };
+      },
+      
+      publisher: async (_, { id }, { loaders }) => loaders.publisher.one(id),
 };
 
 export default publisherQueries;
